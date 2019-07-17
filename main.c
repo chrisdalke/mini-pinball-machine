40f732a7347e8ba69212fbeb87a45c37ce6c7955
Chris Dalke <chrisdalke@gmail.com>
2019-07-17 13:37:44 -0400

    Basic physics with balls

--- main.c
+++ main.c
@@ -16,7 +16,34 @@ long long millis() {
     return milliseconds;
 }
 
+typedef struct {
+    cpShape *shape;
+    cpBody *body;
+} Ball;
+
+typedef struct {
+    cpSpace *space;
+    int numBalls;
+    Ball *balls;
+} GameStruct;
+
 const int numWalls = 64;
+const int maxBalls = 16;
+const float ballSize = 5;
+
+// Add ball function
+void addBall(GameStruct *game, float px, float py){
+    if (game->numBalls < maxBalls){
+        game->numBalls++;
+        float radius = ballSize / 2.0;
+        float mass = 1.0;
+        cpFloat moment = cpMomentForCircle(mass, 0, radius, cpvzero);
+        game->balls[game->numBalls - 1].body = cpSpaceAddBody(game->space,cpBodyNew(mass,moment));
+        cpBodySetPosition(game->balls[game->numBalls - 1].body,cpv(px,py));
+        game->balls[game->numBalls - 1].shape = cpSpaceAddShape(game->space,cpCircleShapeNew(game->balls[game->numBalls - 1].body,radius,cpvzero));
+        cpShapeSetFriction(game->balls[game->numBalls - 1].shape,0.7);
+    }
+}
 
 // Write circle wall segments in a clockwise direction starting at degStart and ending at degEnd
 void writeCircleWallSegment(float walls[numWalls][4],int segmentIndex,int numSegments,float degStart,float degEnd,float centerX, float centerY, float radius){
@@ -51,7 +78,9 @@ int main(void){
     const float worldToScreen = (float)screenWidth / (float)worldWidth;
     const float screenToWorld = (float)worldWidth / (float)screenWidth;
 
-    const float ballSize = 5;
+    // Initialize a struct encoding data about the game.
+    GameStruct game;
+
 
     float walls[64][4] = {
         {0,0,worldWidth,0},
@@ -78,17 +107,58 @@ int main(void){
     SetTargetFPS(60);
 
     Texture bgTex = LoadTexture("Resources/Textures/background2.png");
+    Texture ballTex = LoadTexture("Resources/Textures/ball.png");
 
     // Initialize physics simulation
-    cpVect gravity = cpv(0,-100);
+    cpVect gravity = cpv(0,100);
+    cpSpace *space = cpSpaceNew();
+    game.space = space;
+    cpSpaceSetGravity(space,gravity);
+    cpFloat timeStep = 1.0/60.0;
 
+    // create walls
+    for (int i = 0; i < numWalls; i++){
+        cpShape *wall = cpSegmentShapeNew(cpSpaceGetStaticBody(space),cpv(walls[i][0],walls[i][1]),cpv(walls[i][2],walls[i][3]),0);
+        cpShapeSetFriction(wall,0);
+        cpSpaceAddShape(space,wall);
+    }
+
+    //create balls array
+    Ball* balls = malloc(maxBalls * sizeof(Ball));
+    game.balls = balls;
+
+    // Setup timestepping system
+    int timestep = 1000.0/60.0;
+    long long accumulatedTime = 0;
+    long long startTime = millis();
+    long long endTime = millis();
 
     while (!WindowShouldClose()){
+        endTime = millis();
+        accumulatedTime += (endTime - startTime);
+        startTime = millis();
+
+        // STEP SIMULATION AT FIXED RATE
+        while (accumulatedTime > timestep){
+            accumulatedTime -= timestep;
+            cpSpaceStep(space, timeStep);
+
+            if (IsKeyPressed(KEY_SPACE)){
+                addBall(&game,70,50);
+            }
+        }
+
+        // RENDER AT SPEED GOVERNED BY RAYLIB
         BeginDrawing();
         DrawTexturePro(bgTex,(Rectangle){0,0,bgTex.width,bgTex.height},(Rectangle){0,0,screenWidth,screenHeight},(Vector2){0,0},0,WHITE);
 
+        // Render balls
+        for (int i = 0; i < game.numBalls; i++){
+            cpVect pos = cpBodyGetPosition(balls[i].body);
+            DrawTexturePro(ballTex,(Rectangle){0,0,ballTex.width,ballTex.height},(Rectangle){pos.x * worldToScreen,pos.y * worldToScreen,ballSize * worldToScreen,ballSize * worldToScreen},(Vector2){(ballSize / 2.0) * worldToScreen,(ballSize / 2.0) * worldToScreen},0,WHITE);
+        }
 
-
+        // DEBUG RENDERING
         if (IsKeyDown(KEY_TAB)){
             DrawFPS(10, 10);
             DrawText("MINI PINBALL BY CHRIS DALKE", 10, 40, 20, GRAY);
@@ -101,13 +171,11 @@ int main(void){
                 printf("{%f,%f,,},\n",(float)(mouseX * screenToWorld),(float)(mouseY * screenToWorld));
             }
 
-
             for (int i = 0; i < numWalls; i++){
                 DrawLineEx((Vector2){walls[i][0]*worldToScreen,walls[i][1]*worldToScreen},(Vector2){walls[i][2]*worldToScreen,walls[i][3]*worldToScreen},1,GREEN);
                 DrawCircle(walls[i][0]*worldToScreen,walls[i][1]*worldToScreen,2,RED);
                 DrawCircle(walls[i][2]*worldToScreen,walls[i][3]*worldToScreen,2,RED);
             }
-
         }
 
         EndDrawing();
