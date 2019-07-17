3f1368199f4178af8cd55e6ff9cfefa85f1cd4a2
Chris Dalke <chrisdalke@gmail.com>
2019-07-16 21:05:29 -0400

    bg, basic collision geometry

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/time.h>
 #include <math.h>
 
@@ -14,6 +15,32 @@ long long millis() {
     return milliseconds;
 }
 
+const int numWalls = 64;
+
+// Write circle wall segments in a clockwise direction starting at degStart and ending at degEnd
+void writeCircleWallSegment(float walls[numWalls][4],int segmentIndex,int numSegments,float degStart,float degEnd,float centerX, float centerY, float radius){
+    float totalDegreeRange = fabsf(degEnd - degStart);
+    float degPerPoint = totalDegreeRange / (numSegments + 1);
+
+    degStart -= 90;
+
+    // Iterate through numSegments + 1 points.
+    float prevPointX = centerX + (cos(degStart * DEG_TO_RAD) * radius);
+    float prevPointY = centerY + (sin(degStart * DEG_TO_RAD) * radius);
+    float curPointX = 0;
+    float curPointY = 0;
+    for (int i = 1; i <= numSegments; i++){
+        curPointX = centerX + (cos((degStart + (i * degPerPoint)) * DEG_TO_RAD) * radius);
+        curPointY = centerY + (sin((degStart + (i * degPerPoint)) * DEG_TO_RAD) * radius);
+        walls[segmentIndex + i - 1][0] = prevPointX;
+        walls[segmentIndex + i - 1][1] = prevPointY;
+        walls[segmentIndex + i - 1][2] = curPointX;
+        walls[segmentIndex + i - 1][3] = curPointY;
+        prevPointX = curPointX;
+        prevPointY = curPointY;
+    }
+}
+
 int main(void){
     const int screenWidth = 450;
     const int screenHeight = 800;
@@ -21,9 +48,25 @@ int main(void){
     const int worldHeight = 160;
 
     const float worldToScreen = (float)screenWidth / (float)worldWidth;
+    const float screenToWorld = (float)worldWidth / (float)screenWidth;
 
-    int numWalls = 1;
-    float walls[1][4] = {{0,0,100,100}};
+    const float ballSize = 5;
+
+    float walls[numWalls][4] = {
+        {0,0,worldWidth,0},
+        {0,0,0,worldHeight},
+        {worldWidth,0,worldWidth,worldHeight},
+        {worldWidth - 6,56,worldWidth - 6,worldHeight},
+        {worldWidth - 7,56,worldWidth - 7,worldHeight},
+        {worldWidth-6,56,worldWidth-7,56},
+        {0,128,19,142},
+        {worldWidth - 7,128,worldWidth - 26,142},
+        {0,2.1,worldWidth,2.1},
+        {40.4,1.6,41.2,4.0},
+        {41.2,4.0,65.2,1.6},
+    };
+    writeCircleWallSegment(walls,11,20,0,90,worldWidth-28.5,30.75,28.75);
+    writeCircleWallSegment(walls,31,20,270,360,28.5,30.75,28.75);
 
     InitAudioDevice();
     Sound sound = LoadSound("Resources/Audio/1.mp3");
@@ -39,18 +82,30 @@ int main(void){
     while (!WindowShouldClose()){
         BeginDrawing();
         DrawTexturePro(bgTex,(Rectangle){0,0,bgTex.width,bgTex.height},(Rectangle){0,0,screenWidth,screenHeight},(Vector2){0,0},0,WHITE);
-        DrawFPS(10, 10);
-        DrawText("MINI PINBALL BY CHRIS DALKE", 10, 40, 20, GRAY);
-        DrawLine(0,0,0,screenHeight,BLUE);
-        DrawLine(screenWidth,0,screenWidth,screenHeight,BLUE);
 
 
-        for (int i = 0; i < 1; i++){
-            DrawLineEx((Vector2){walls[i][0]*worldToScreen,walls[i][1]*worldToScreen},(Vector2){walls[i][2]*worldToScreen,walls[i][3]*worldToScreen},4,GREEN);
+
+        if (IsKeyDown(KEY_TAB)){
+            DrawFPS(10, 10);
+            DrawText("MINI PINBALL BY CHRIS DALKE", 10, 40, 20, GRAY);
+            
+            float mouseX = GetMouseX() * 2.0;
+            float mouseY = GetMouseY() * 2.0;
+            DrawLine(0,mouseY,screenWidth,mouseY,RED);
+            DrawLine(mouseX,0,mouseX,screenHeight,RED);
+            if (IsMouseButtonPressed(0)){
+                printf("{%f,%f,,},\n",(float)(mouseX * screenToWorld),(float)(mouseY * screenToWorld));
+            }
+
+
+            for (int i = 0; i < numWalls; i++){
+                DrawLineEx((Vector2){walls[i][0]*worldToScreen,walls[i][1]*worldToScreen},(Vector2){walls[i][2]*worldToScreen,walls[i][3]*worldToScreen},1,GREEN);
+                DrawCircle(walls[i][0]*worldToScreen,walls[i][1]*worldToScreen,2,RED);
+                DrawCircle(walls[i][2]*worldToScreen,walls[i][3]*worldToScreen,2,RED);
+            }
+
         }
 
-
-        DrawCircle(screenWidth/2 + sin(millis() / 500.0) * 100,screenHeight/2 + cos(millis() / 500.0) * 100,20,RED);
         EndDrawing();
     }
 
